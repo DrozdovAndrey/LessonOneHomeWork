244ae4081104d15bad7a30b6a8d7b77a07443844
Andrey Drozdov <drozdov_andrey@list.ru>
2022-06-26 14:48:58 +0300

    Added Task06 done

---
+++ Task06/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace HelloWorld  
+// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Введите целое число: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int even;
+            int odd;
+            if (number % 2 == 0)
+                {
+                    even = number;
+                    Console.WriteLine($"Введенное число - {even} - четное");
+                }
+            else
+                {
+                    odd = number;
+                    Console.WriteLine($"Введенное число - {odd} - нечетное");
+                }    
+                
+        }  
+    }
+}
