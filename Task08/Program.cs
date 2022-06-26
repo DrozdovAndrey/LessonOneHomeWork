f1658437ca93bb8fc1b9d781b1922fedd2b3b441
Andrey Drozdov <drozdov_andrey@list.ru>
2022-06-26 15:26:59 +0300

    Task08 added changes

--- Task08/Program.cs
+++ Task08/Program.cs
@@ -10,16 +10,26 @@
         static void Main(string[] args)
         {
             Console.Clear();
-            Console.WriteLine("Введите число ");
+            Console.WriteLine("Введите число от 1: ");
             double number = Convert.ToDouble(Console.ReadLine());
-            int odd = 0;
-            Console.WriteLine($"Все четные числа от 1 до {number}: ");
-            while (odd < number)
+            int odd = 1;
+            if (number < 1)
                 {
-                    if (odd % 2 == 0) Console.WriteLine($"{odd}");
-                    odd +=2;
+                    Console.WriteLine("Введено некорректное значение! Повторите ввод от 1!");
                 }
+            else
+                {
+                    Console.WriteLine($"Все четные числа от 1 до {number} (не включая {number}): ");
+                    while (odd < number)
+                    {
+                        if (odd % 2 == 0)  Console.WriteLine($"{odd}");
+                        odd +=1;     
+                    }
+                    if (odd == 1) Console.WriteLine("В этом дапозоне нет четных чисел! Введите другое число!");
+                    if (odd == 2) Console.WriteLine("В этом дапозоне нет четных чисел! Введите другое число!");
 
+                }
+            
                 
         }  
     }
