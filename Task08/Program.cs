b42fd9f3ba7f01c338f238e4e35e0b47a6af4c7b
Andrey Drozdov <drozdov_andrey@list.ru>
2022-06-26 15:08:17 +0300

    Added Task08 done

---
+++ Task08/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace HelloWorld  
+// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N. (не включая N)
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Введите число ");
+            double number = Convert.ToDouble(Console.ReadLine());
+            int odd = 0;
+            Console.WriteLine($"Все четные числа от 1 до {number}: ");
+            while (odd < number)
+                {
+                    if (odd % 2 == 0) Console.WriteLine($"{odd}");
+                    odd +=2;
+                }
+
+                
+        }  
+    }
+}
